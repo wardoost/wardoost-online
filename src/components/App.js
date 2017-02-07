693705d54b8871be55b16843061d4c33aba95199
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2017-02-07 21:58:38 +1100

    Scroll animation page transition

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+import {scrollTop} from '../utils/scroll'
 import Layout from './common/Layout'
 import Home from './pages/Home'
 import Buttons from './pages/Buttons'
@@ -7,9 +8,15 @@ import Error from './pages/Error'
 import './App.scss'
 
 export default class App extends Component {
+  handleUpdate () {
+    if (this.state.location.action === 'PUSH') {
+      scrollTop(500)
+    }
+  }
+
   render () {
     return (
-      <Router history={browserHistory}>
+      <Router history={browserHistory} onUpdate={this.handleUpdate}>
         <Route path='/' component={Layout}>
           <IndexRoute component={Home} />
           <Route path='buttons' component={Buttons} />
---
+++ src/utils/scroll.js
@@ -0,0 +1,31 @@
+export function scrollTop (duration) {
+  scrollTo(document.body, 0, duration)
+}
+
+export function scrollTo (element, to, duration) {
+  const start = element.scrollTop
+  const change = to - start
+  const increment = 20
+
+  const animateScroll = elapsedTime => {
+    elapsedTime += increment
+    var position = easeInOut(elapsedTime, start, change, duration)
+    element.scrollTop = position
+    if (elapsedTime < duration) {
+      setTimeout(() => {
+        animateScroll(elapsedTime)
+      }, increment)
+    }
+  }
+
+  animateScroll(0)
+}
+
+function easeInOut (currentTime, start, change, duration) {
+  currentTime /= duration / 2
+  if (currentTime < 1) {
+    return change / 2 * currentTime * currentTime + start
+  }
+  currentTime -= 1
+  return -change / 2 * (currentTime * (currentTime - 2) - 1) + start
+}
