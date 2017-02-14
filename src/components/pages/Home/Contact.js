1eba79f9826875632a86a4a509726e46a6b7fedc
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2017-02-14 17:31:16 +1100

    Contact form added

--- src/components/pages/Home/Contact.js
+++ src/components/pages/Home/Contact.js
@@ -1,7 +1,7 @@
 import React, {PureComponent} from 'react'
 import CSSModules from 'react-css-modules'
 import FaEnvelope from 'react-icons/lib/fa/envelope'
-import {Grid, Unit} from '../../ui'
+import {Grid, Unit, Form, Input, Button} from '../../ui'
 import styles from './Contact.scss'
 
 @CSSModules(styles)
@@ -16,18 +16,30 @@ export default class Contact extends PureComponent {
 
     return (
       <div {...props}>
-        <div styleName='contact-header' className='section-animated-bg'>
+        <div className='section-animated-bg' styleName='contact-header'>
           <h1>Contact</h1>
-          <p>You want to hire me, work with me or just have something awesome to show?</p>
+          <p>You want to hire me, work with me or just have something awesome to show? Send me an email or fill the contact form</p>
         </div>
         <Grid padding='xs'>
-          <Unit size='1' styleName='mail'>
-            <div className='section-animated-bg'>
+          <Unit size='1' smSize='1-5' styleName='mail'>
+            <div className='section-animated-bg' style={{transitionDelay: `0.1s`}}>
               <a styleName='mail' href='mailto:wardoosterlijnck@gmail.com' target='_blank' title='Send me an email'>
                 <div styleName='icon'>
                   <FaEnvelope />
                 </div>
               </a>
+            </div>
+          </Unit>
+          <Unit size='1' smSize='4-5' styleName='contact-form'>
+            <div className='section-animated-bg' style={{transitionDelay: `0.2s`}}>
+              <Form kind='stacked'>
+                <fieldset>
+                  <Input type='text' placeholder='Name' size='2-3' />
+                  <Input type='email' placeholder='Email' size='2-3' required />
+                  <textarea type='text' placeholder='Message' required />
+                  <Button type='submit' kind='primary'>Send</Button>
+                </fieldset>
+              </Form>
             </div>
           </Unit>
         </Grid>
