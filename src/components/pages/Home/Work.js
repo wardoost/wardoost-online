d4b580199320ccc9c42e60a034ad7375dcfc9940
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2017-02-22 11:22:09 +1100

    Work section alteration

--- src/components/pages/Home/Work.js
+++ src/components/pages/Home/Work.js
@@ -7,10 +7,63 @@ import tinderMeCards from '../../../assets/tinderme-cards.jpg'
 import shootingTheApes from '../../../assets/shooting-the-apes.jpg'
 import styles from './Work.scss'
 
+const works = [
+  {
+    title: 'Oïnk Agency',
+    description: 'Loved working here for more than 3 years.',
+    image: oink,
+    link: 'https://www.oink.agency/',
+    linkLabel: 'www.oink.agency'
+  },
+  {
+    title: 'TinderMe Cards',
+    description: 'Website to create business cards of your <a href="https://www.gotinder.com/">Tinder</a> profile.',
+    image: tinderMeCards,
+    link: 'http://www.tinderme.cards/',
+    linkLabel: 'www.tinderme.cards'
+  },
+  {
+    title: 'Shooting the Apes',
+    description: 'Collective I co-created with <a href="http://www.jeroensmans.be/">Jeroen Smans</a> to release event movies for concerts and festivals.',
+    image: shootingTheApes,
+    link: 'https://vimeo.com/shootingtheapes',
+    linkLabel: 'Vimeo profile'
+  }
+]
+
 @CSSModules(styles)
 export default class Work extends PureComponent {
   static propTypes = {
     active: React.PropTypes.bool
+  }
+
+  createWorkItems (active) {
+    return works.map((item, i) => {
+      const {title, description, image, link, linkLabel} = item
+
+      return (
+        <Unit key={i} size='1' smSize='1-2' lgSize='1-3'>
+          <div className='section-animated-bg' style={{transitionDelay: `${active ? 0.1 * (i + 1) : 0.1 * (works.length - i)}s`}}>
+            <div styleName='work-item'>
+              <h2>{title}</h2>
+              <Grid padding='md'>
+                <Unit size='1-3' smSize='1' mdSize='1-3'>
+                  <Image src={image} alt={title} styleName='work-img' />
+                </Unit>
+                <Unit size='2-3' smSize='1' mdSize='2-3'>
+                  <p styleName='work-description' dangerouslySetInnerHTML={{ __html: description }} />
+                </Unit>
+              </Grid>
+            </div>
+            <div styleName='work-link'>
+              <a href={link} target='_blank'>
+                <FaChain /> {linkLabel}
+              </a>
+            </div>
+          </div>
+        </Unit>
+      )
+    })
   }
 
   render () {
@@ -22,60 +75,7 @@ export default class Work extends PureComponent {
           <h1>Work</h1>
         </div>
         <Grid padding='xs'>
-          <Unit size='1' smSize='1-2' lgSize='1-3'>
-            <div styleName='work-item' className='section-animated-bg' style={{transitionDelay: `0.${active ? '1' : '3'}s`}}>
-              <h2>Oïnk Agency</h2>
-              <Grid padding='md'>
-                <Unit size='1-3' smSize='1' mdSize='1-3'>
-                  <Image src={oink} styleName='work-img' />
-                </Unit>
-                <Unit size='2-3' smSize='1' mdSize='2-3'>
-                  <p styleName='work-description'>
-                    Loved working here for more than 3 years.
-                  </p>
-                  <p styleName='work-link'>
-                    <FaChain /> <a href='https://www.oink.agency/'>www.oink.agency</a>
-                  </p>
-                </Unit>
-              </Grid>
-            </div>
-          </Unit>
-          <Unit size='1' smSize='1-2' lgSize='1-3'>
-            <div styleName='work-item' className='section-animated-bg' style={{transitionDelay: '0.2s'}}>
-              <h2>TinderMe Cards</h2>
-              <Grid padding='md'>
-                <Unit size='1-3' smSize='1' mdSize='1-3'>
-                  <Image src={tinderMeCards} styleName='work-img' />
-                </Unit>
-                <Unit size='2-3' smSize='1' mdSize='2-3'>
-                  <p styleName='work-description'>
-                    Website to create business cards of your <a href='https://www.gotinder.com/'>Tinder</a> profile.
-                  </p>
-                  <p styleName='work-link'>
-                    <FaChain /> <a href='http://www.tinderme.cards/'>www.tinderme.cards</a>
-                  </p>
-                </Unit>
-              </Grid>
-            </div>
-          </Unit>
-          <Unit size='1' smSize='1-2' lgSize='1-3'>
-            <div styleName='work-item' className='section-animated-bg' style={{transitionDelay: `0.${active ? '3' : '1'}s`}}>
-              <h2>Shooting the Apes</h2>
-              <Grid padding='md'>
-                <Unit size='1-3' smSize='1' mdSize='1-3'>
-                  <Image src={shootingTheApes} styleName='work-img' />
-                </Unit>
-                <Unit size='2-3' smSize='1' mdSize='2-3'>
-                  <p styleName='work-description'>
-                    Collective I co-created with <a href='http://www.jeroensmans.be/'>Jeroen Smans</a> to release event movies for concerts and festivals.
-                  </p>
-                  <p styleName='work-link'>
-                    <FaChain /> <a href='https://vimeo.com/shootingtheapes'>Vimeo profile</a>
-                  </p>
-                </Unit>
-              </Grid>
-            </div>
-          </Unit>
+          {this.createWorkItems(active)}
         </Grid>
       </div>
     )
