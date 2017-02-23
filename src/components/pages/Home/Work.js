33d5237d2e7d2aa0df3bc06d0f52e957c136a5aa
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2017-02-23 12:19:50 +1100

    Work section update

--- src/components/pages/Home/Work.js
+++ src/components/pages/Home/Work.js
@@ -4,30 +4,49 @@ import {Grid, Unit, Image} from '../../ui'
 import FaChain from 'react-icons/lib/fa/chain'
 import oink from '../../../assets/oink.jpg'
 import tinderMeCards from '../../../assets/tinderme-cards.jpg'
+import storyMeWellPlayed from '../../../assets/storyme-wellplayed.jpg'
 import shootingTheApes from '../../../assets/shooting-the-apes.jpg'
 import styles from './Work.scss'
 
 const works = [
   {
     title: 'Oïnk Agency',
-    description: 'Loved working here for more than 3 years.',
+    description: 'Loved working here for more than 3 years. As it was a small agency when I started I had a broad range of responsibilities. Creating EDM workflows to thinking up social media campaigns to managing a small dev team. Good for gaining a lot of experience really fast.',
     image: oink,
-    link: 'https://www.oink.agency/',
-    linkLabel: 'www.oink.agency'
+    link: {
+      url: 'https://www.oink.agency/',
+      label: 'oink.agency'
+    }
   },
   {
     title: 'TinderMe Cards',
-    description: 'Website to create business cards of your <a href="https://www.gotinder.com/">Tinder</a> profile.',
+    description: 'Website to create business cards of your <a href="https://www.gotinder.com/" target="_blank">Tinder</a> profile. Go create your own and make you business life a little more exciting!',
     image: tinderMeCards,
-    link: 'http://www.tinderme.cards/',
-    linkLabel: 'www.tinderme.cards'
+    link: {
+      url: 'http://www.tinderme.cards/',
+      label: 'tinderme.cards'
+    }
+  },
+  {
+    title: 'StoryMe & Well Played',
+    description: 'Motion designing short explainer videos as a freelancer for <a href="http://www.storyme.com/" target="_blank">StoryMe</a> and <a href="http://www.wellplayed.video/" target="_blank">Well Played</a>. Saw every hidden panel of After Effects while doing this.',
+    image: storyMeWellPlayed,
+    links: [{
+      url: 'http://www.wellplayed.video/',
+      label: 'wellplayed.video'
+    }, {
+      url: 'http://www.storyme.com/',
+      label: 'storyme.com'
+    }]
   },
   {
     title: 'Shooting the Apes',
-    description: 'Collective I co-created with <a href="http://www.jeroensmans.be/">Jeroen Smans</a> to release event movies for concerts and festivals.',
+    description: 'Collective I co-created with <a href="http://www.jeroensmans.be/" target="_blank">Jeroen Smans</a> to release event movies for concerts and festivals.',
     image: shootingTheApes,
-    link: 'https://vimeo.com/shootingtheapes',
-    linkLabel: 'Vimeo profile'
+    link: {
+      url: 'https://vimeo.com/shootingtheapes',
+      label: 'Vimeo profile'
+    }
   }
 ]
 
@@ -37,29 +56,54 @@ export default class Work extends PureComponent {
     active: React.PropTypes.bool
   }
 
+  createWorkLinks (links) {
+    if (links) {
+      return (
+        <div styleName='work-links'>
+          <Grid>
+            {links.map((link, i) => {
+              return (
+                <Unit key={i} smSize={links.length % 2 !== 0 && links.length - i === 1 ? '1' : '1-2'}>
+                  <div styleName='work-link'>
+                    <a href={link.url} target='_blank'>
+                      <FaChain /> {link.label || link.url}
+                    </a>
+                  </div>
+                </Unit>
+              )
+            })}
+          </Grid>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
   createWorkItems (active) {
     return works.map((item, i) => {
-      const {title, description, image, link, linkLabel} = item
+      const {title, description, image} = item
+      let links = item.link ? [item.link] : item.links
 
       return (
-        <Unit key={i} size='1' smSize='1-2' lgSize='1-3'>
-          <div className='section-animated-bg' style={{transitionDelay: `${active ? 0.1 * (i + 1) : 0.1 * (works.length - i)}s`}}>
-            <div styleName='work-item'>
+        <Unit key={i} size='1' mdSize='1-2' xlSize='1-3'>
+          <div
+            className='section-animated-bg'
+            style={{transitionDelay: `${active ? 100 * (i + 1) : 100 * (works.length - i)}ms`}}>
+            <div
+              styleName='work'
+              style={{paddingBottom: 20 + (Math.floor(links.length / 2) + links.length % 2) * 65}}>
               <h2>{title}</h2>
               <Grid padding='md'>
-                <Unit size='1-3' smSize='1' mdSize='1-3'>
+                <Unit smSize='1-3'>
                   <Image src={image} alt={title} styleName='work-img' />
                 </Unit>
-                <Unit size='2-3' smSize='1' mdSize='2-3'>
+                <Unit smSize='2-3'>
                   <p styleName='work-description' dangerouslySetInnerHTML={{ __html: description }} />
                 </Unit>
               </Grid>
             </div>
-            <div styleName='work-link'>
-              <a href={link} target='_blank'>
-                <FaChain /> {linkLabel}
-              </a>
-            </div>
+            {this.createWorkLinks(links)}
           </div>
         </Unit>
       )
