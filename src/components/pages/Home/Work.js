6b41f061db0bc92fda3b30f8ebad6a75347469b5
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2017-02-23 17:00:44 +1100

    Added ‘when’ field to work items

--- src/components/pages/Home/Work.js
+++ src/components/pages/Home/Work.js
@@ -11,7 +11,8 @@ import styles from './Work.scss'
 const works = [
   {
     title: 'Oïnk Agency',
-    description: 'Loved working here for more than 3 years. As it was a small agency when I started I had a broad range of responsibilities. Creating EDM workflows to thinking up social media campaigns to managing a small dev team. Good for gaining a lot of experience really fast.',
+    when: '2013-2016',
+    description: 'Loved working here for more than 3 years. As it was a small agency when I started I dealt with a broad range of responsibilities over the years. Creating EDM workflows to thinking up social media campaigns to managing a small dev team. Good for gaining a lot of experience really fast.',
     image: oink,
     link: {
       url: 'https://www.oink.agency/',
@@ -20,7 +21,8 @@ const works = [
   },
   {
     title: 'TinderMe Cards',
-    description: 'Website to create business cards of your <a href="https://www.gotinder.com/" target="_blank">Tinder</a> profile. Go create your own and make you business life a little more exciting!',
+    when: '2016',
+    description: 'Website created in collaboration with <a href="http://driesdepoorter.be/" target="_blank">Dries Depoorter</a> to generate personalised <a href="https://www.gotinder.com/" target="_blank">Tinder</a> business cards. Go create your own and make you business life a little more exciting!',
     image: tinderMeCards,
     link: {
       url: 'http://www.tinderme.cards/',
@@ -29,6 +31,7 @@ const works = [
   },
   {
     title: 'StoryMe & Well Played',
+    when: '2012-2013',
     description: 'Motion designing short explainer videos as a freelancer for <a href="http://www.storyme.com/" target="_blank">StoryMe</a> and <a href="http://www.wellplayed.video/" target="_blank">Well Played</a>. Saw every hidden panel of After Effects while doing this.',
     image: storyMeWellPlayed,
     links: [{
@@ -41,6 +44,7 @@ const works = [
   },
   {
     title: 'Shooting the Apes',
+    when: '2011-2014',
     description: 'Collective I co-created with <a href="http://www.jeroensmans.be/" target="_blank">Jeroen Smans</a> to release event movies for concerts and festivals.',
     image: shootingTheApes,
     link: {
@@ -82,7 +86,7 @@ export default class Work extends PureComponent {
 
   createWorkItems (active) {
     return works.map((item, i) => {
-      const {title, description, image} = item
+      const {title, when, description, image} = item
       let links = item.link ? [item.link] : item.links
 
       return (
@@ -92,14 +96,15 @@ export default class Work extends PureComponent {
             style={{transitionDelay: `${active ? 100 * (i + 1) : 100 * (works.length - i)}ms`}}>
             <div
               styleName='work'
-              style={{paddingBottom: 20 + (Math.floor(links.length / 2) + links.length % 2) * 65}}>
-              <h2>{title}</h2>
+              style={{paddingBottom: (Math.floor(links.length / 2) + links.length % 2) * 65 - 10}}>
+              <h2 styleName='work-title'>{title}</h2>
+              <p styleName='work-when'>{when}</p>
               <Grid padding='md'>
-                <Unit smSize='1-3'>
+                <Unit smSize='1-3' mdSize='1' lgSize='1-3'>
                   <Image src={image} alt={title} styleName='work-img' />
                 </Unit>
-                <Unit smSize='2-3'>
-                  <p styleName='work-description' dangerouslySetInnerHTML={{ __html: description }} />
+                <Unit smSize='2-3' mdSize='1' lgSize='2-3' styleName='work-description'>
+                  <p dangerouslySetInnerHTML={{ __html: description }} />
                 </Unit>
               </Grid>
             </div>
