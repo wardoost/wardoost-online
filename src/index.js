95de4b25622cec0a1d5af509cd69b5b1193d4e96
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2019-09-03 22:27:58 +1000

    Remove breaking disable React devtools snippet

--- src/index.js
+++ src/index.js
@@ -5,14 +5,6 @@ import App from './components/App'
 import {AppContainer} from 'react-hot-loader'
 
 if (process.env.NODE_ENV !== 'development') {
-  // Disable React devtools
-  if (
-    window.__REACT_DEVTOOLS_GLOBAL_HOOK__ &&
-    Object.keys(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers).length
-  ) {
-    window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers = {}
-  }
-
   // Enable service worker
   require('./pwa')
 }
